bc2e0d6afd0aca8ac6585ede7e69a63028316db2
Raj Paliwad <infinitie.raj@gmail.com>
2026-04-24 17:07:57 +0530

    my first push to dev

--- lib/features/splash/view/splash_screen.dart
+++ lib/features/splash/view/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gmb_iq/core/widgets/custom_scaffold.dart';
 import 'package:go_router/go_router.dart';
 import '../../../core/responsive/responsive_widget.dart';
 import '../../../core/router/app_router.dart';
@@ -15,20 +16,6 @@ class SplashScreen extends StatelessWidget {
   }
 
   Widget _buildUI(BuildContext context, String text) {
-    return Scaffold(
-      // backgroundColor: Theme.of(context).primaryColor,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.flash_on, size: 80, color: Colors.white),
-            const SizedBox(height: 16),
-            Text(text, style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 24),
-            ElevatedButton(onPressed: () => context.goNamed(AppRoutes.home), child: const Text('Go to Hasome')),
-          ],
-        ),
-      ),
-    );
+    return CustomScaffold(body: Column());
   }
 }
